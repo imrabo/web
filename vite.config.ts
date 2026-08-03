e8bf03221c975a3fed5b7579e5f25e837e010312
iamprathameshmore <iamprathameshmore07@gmail.com>
2026-08-03 18:44:01 +0530

    feat(main): added pwa with vite for reactjs application

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,66 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      injectRegister: 'auto',
+
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'robots.txt'
+      ],
+
+      manifest: {
+        name: 'My App',
+        short_name: 'MyApp',
+        description: 'My React PWA',
+        theme_color: '#1976d2',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        lang: 'en',
+
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg,json}'
+        ]
+      }
+    })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 })
